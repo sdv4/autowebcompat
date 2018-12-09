8449568aa64fcdb19071b021c35c26615976116c
shanesims <shane.sims.ss@gmail.com>
2018-12-09 13:00:26 -0800

    changed target_size in utils.py to 224,224

--- autowebcompat/utils.py
+++ autowebcompat/utils.py
@@ -81,17 +81,22 @@ images = {}
 
 
 def load_image(fname, parent_dir='data_resized'):
-    global images
-
-    if fname in images:
-        return images[fname]
-
-    img = load_img(os.path.join(parent_dir, fname), target_size=(32, 24))
+    img = load_img(os.path.join(parent_dir, fname), target_size=(224, 224))
     x = img_to_array(img, data_format=keras.backend.image_data_format())
-
-    images[fname] = x
-
     return x
+
+#def load_image(fname, parent_dir='data_resized'):
+#    global images
+#
+#    if fname in images:
+#        return images[fname]
+#
+#    img = load_img(os.path.join(parent_dir, fname), target_size=(224, 224))
+#    x = img_to_array(img, data_format=keras.backend.image_data_format())
+#
+#    images[fname] = x
+#
+#    return x
 
 
 def get_ImageDataGenerator(images, image_shape, parent_dir='data_resized'):
